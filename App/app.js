2235690fc4d5de40538a520fd9b86e30af449914
Aimilios Hatzistamou <aimilios@wisepricing.com>
2019-01-26 18:07:39 +0000

    further work on checkout flow

--- App/app.js
+++ App/app.js
@@ -7,7 +7,10 @@ var animating; //flag to prevent quick multi-click glitches
 $('.scan-button').click(function(){
 	$('.scan-loader').show()
 	$('.scan-button').hide()
-	gotonext($('.scanning-page'))
+	setTimeout(function(){gotonext($('.scanning-page'))}, 4000)
+	// gotonext($('.item-confirmation-page'))
+})
+$('.pay-button').click(function(){
 	gotonext($('.item-confirmation-page'))
 })
 function gotonext(current_page){
@@ -41,42 +44,6 @@ function gotonext(current_page){
 	});
 
 }
-
-// $(".previous").click(function(){
-// 	if(animating) return false;
-// 	animating = true;
-	
-// 	current_fs = $(this).parent();
-// 	previous_fs = $(this).parent().prev();
-	
-// 	//de-activate current step on progressbar
-// 	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-// 	//show the previous fieldset
-// 	previous_fs.show(); 
-// 	//hide the current fieldset with style
-// 	current_fs.animate({opacity: 0}, {
-// 		step: function(now, mx) {
-// 			//as the opacity of current_fs reduces to 0 - stored in "now"
-// 			//1. scale previous_fs from 80% to 100%
-// 			scale = 0.8 + (1 - now) * 0.2;
-// 			//2. take current_fs to the right(50%) - from 0%
-// 			left = ((1-now) * 50)+"%";
-// 			//3. increase opacity of previous_fs to 1 as it moves in
-// 			opacity = 1 - now;
-// 			current_fs.css({'left': left});
-// 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-// 		}, 
-// 		duration: 800, 
-// 		complete: function(){
-// 			current_fs.hide();
-// 			animating = false;
-// 		}, 
-// 		//this comes from the custom easing plugin
-// 		easing: 'easeInOutBack'
-// 	});
-// });
-
 $(".submit").click(function(){
 	return false;
 })
